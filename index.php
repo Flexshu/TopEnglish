b149ea023945096182b5e47ec04f25a321222a2f
Flexshu <egor.wm202@gmail.com>
2026-04-27 12:13:56 +0300

    Did SEO

--- index.php
+++ index.php
@@ -17,6 +17,18 @@ switch($url) {
     case "/login":
         require "templates/login.html";
         break;
+    case "/favicon.ico":
+        header("Content-Type: image/x-icon");
+        require "favicon.ico";
+        break;
+    case "/sitemap.xml":
+        header("Content-Type: text/xml");
+        require "sitemap.xml";
+        break;
+    case "/robots.txt":
+        header("Content-Type: text/plain");
+        require "robots.txt";
+        break;
     default:
         http_response_code(404);
         break;
